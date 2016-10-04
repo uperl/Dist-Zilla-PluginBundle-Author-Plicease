238c4f91457751019b925d21450da03939a0fe55
Graham Ollis <plicease@cpan.org>
2016-10-04 15:29:51 -0400

    my this is old

--- lib/Dist/Zilla/Plugin/Author/Plicease/Tests.pm
+++ lib/Dist/Zilla/Plugin/Author/Plicease/Tests.pm
@@ -206,7 +206,7 @@ use File::Spec;
 chdir(File::Spec->catdir($FindBin::Bin, File::Spec->updir, File::Spec->updir));
 
 unshift @Test::Strict::MODULES_ENABLING_STRICT,
-  'sips',
+  'ozo',
   'Test2::Bundle::SIPS',
   'Test2::Bundle::Extended';
 note "enabling strict = $_" for @Test::Strict::MODULES_ENABLING_STRICT;
