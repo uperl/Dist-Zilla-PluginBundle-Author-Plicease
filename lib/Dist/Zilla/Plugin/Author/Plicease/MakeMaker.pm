92a1725f3875f0c7d5ee3f05d48f4788334d0150
Graham Ollis <plicease@cpan.org>
2017-08-31 10:32:47 -0400

    improvements to eumm plugin

--- lib/Dist/Zilla/Plugin/Author/Plicease/MakeMaker.pm
+++ lib/Dist/Zilla/Plugin/Author/Plicease/MakeMaker.pm
@@ -3,6 +3,7 @@ package Dist::Zilla::Plugin::Author::Plicease::MakeMaker {
   use 5.014;
   use Moose;
   use namespace::autoclean;
+  use List::Util qw( first );
 
   # ABSTRACT: munge the AUTHOR section
 
@@ -23,11 +24,11 @@ L<Dist::Zilla::PluginBundle::Author::Plicease>
 
   extends 'Dist::Zilla::Plugin::MakeMaker';
 
+  with 'Dist::Zilla::Role::MetaProvider';
+
   around write_makefile_args => sub {
-    my $orig = shift;
-    my $self = shift;
-  
-    my $h = $self->$orig(@_);  
+    my($orig, $self, @args) = @_;
+    my $h = $self->$orig(@args);  
 
     # to prevent any non .pm/.pod files from being installed in lib
     # because shit like this is stuff we ought to have to customize.
@@ -36,6 +37,127 @@ L<Dist::Zilla::PluginBundle::Author::Plicease>
 
     $h;
   };
+
+  around setup_installer => sub {
+    my($orig, $self, @args) = @_;
+    
+    $self->$orig(@args);
+    
+    my $file = first { $_->name eq 'Makefile.PL' } @{ $self->zilla->files };
+    my $mod  = first { $_->name eq 'inc/mymm.pl' } @{ $self->zilla->files };
+    
+    my @content = split /\n/, $file->content;
+
+    # pet-peve1: remove blank lines between use
+    {
+      my $i = 0;
+      while($i<$#content)
+      {
+        if($content[$i] =~ /^(use|#)/)
+        { $i++ }
+        elsif($content[$i] =~ /^\s*$/)
+        { @content = @content[0..($i-1),($i+1)..$#content] }
+        else
+        {
+          my @extra = ('');
+        
+          if($mod)
+          {
+            unshift @extra, 'require "./inc/mymm.pl";';
+          }
+        
+          @content = (
+            @content[0..($i-1)], 
+            @extra, 
+            @content[($i)..$#content]
+          );
+          last;
+        }
+      }
+    }
+    
+    # pet-peve2: squeeze multiple blank lines
+    {
+      my @new;
+      my $last_empty = 0;
+      foreach my $line (@content)
+      {
+        if($line =~ /^\s*$/)
+        {
+          if($last_empty)
+          { next }
+          else
+          {
+            $last_empty = 1;
+          }
+        }
+        else
+        {
+          $last_empty = 0;
+        }
+
+        push @new, $line;
+      }
+      @content = @new;
+    }
+    
+    if($mod)
+    {
+      my $last = pop @content;
+      if($last =~ /^WriteMakefile\(/)
+      {
+        my @new;
+        while(defined $content[0] && $content[0] !~ /\%FallbackPrereqs/)
+        {
+          push @new, shift @content;
+        }
+        
+        if((eval $mod->content) && mymm->can('myWriteMakefile'))
+        {
+          $last = "mymm::my$last";
+        }
+        
+        @content = ( @new, $last );
+      }
+      else
+      {
+        $self->log_fatal("unable to find WriteMakefile in Makefile.PL");
+      }
+    }
+    
+    $file->content(join "\n", @content);
+    
+    return;
+  };
+
+  around register_prereqs => sub {
+    my($orig, $self, @args) = @_;
+    my $h = $self->$orig(@args);  
+
+    my $mod  = first { $_->name eq 'inc/mymm.pl' } @{ $self->zilla->files };
+    if($mod)
+    {
+      $self->zilla->register_prereqs(
+        { phase => 'configure' },
+        'ExtUtils::MakeMaker' => '6.64'
+      );
+    }
+    
+    return;
+  };
+
+  sub metadata
+  {
+    my($self) = @_;
+    
+    my %meta;
+    
+    my $mod  = first { $_->name eq 'inc/mymm.pl' } @{ $self->zilla->files };
+    
+    $meta{dynamic_config} = 1 if $mod;
+
+    \%meta;
+  }
 
   __PACKAGE__->meta->make_immutable;
 }
