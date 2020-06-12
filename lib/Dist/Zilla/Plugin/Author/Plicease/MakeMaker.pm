19b2ed1b5653391db2d90c6443b590ba1ef7eb5a
Graham Ollis <plicease@cpan.org>
2020-06-12 07:39:55 -0600

    syntax oopsie

--- lib/Dist/Zilla/Plugin/Author/Plicease/MakeMaker.pm
+++ lib/Dist/Zilla/Plugin/Author/Plicease/MakeMaker.pm
@@ -177,7 +177,7 @@ L<Dist::Zilla::PluginBundle::Author::Plicease>
 
     if($config || $build || $test || $clean)
     {
-      push @content, "{ package"
+      push @content, "{ package";
       push @content, "    My;";
       push @content, "  sub postamble {";
       push @content, "    my \$postamble = '';";
